344f9a4d75cff6bbd34ec3a9f736cbbe425c4bbd
sm4 <sm4@kul.is>
2016-04-10 11:35:16 +1000

    Refactored the request

--- app/tcrequest.js
+++ app/tcrequest.js
@@ -1,38 +1,47 @@
-var fs = require('fs');
+"use strict";
+
 var http = require('http');
 
-var conf;
-
-var Request = function (method, path) {
-    this.host = conf.hostname;
-    this.port = conf.port;
-    this.auth = conf.username + ':' + conf.password;
+var Request = function (method, path, postdata) {
+    this.host = taconf.hostname;
+    this.port = taconf.port;
+    this.auth = taconf.username + ':' + taconf.password;
     this.headers = {
         'Content-type': 'application/xml',
         'Accept': 'application/json'
     };
     this.method = method;
     this.path = path;
+    this.postdata = postdata;
 
     this.execute = function (callback) {
-        http.request(this, function (response) {
+        var req = http.request(this, function (response) {
             response.setEncoding('utf8');
             var body = '';
             response.on('data', function (chunk) {
                 body += chunk;
             });
             response.on('end', function () {
-                var json = JSON.parse(body);
-                callback(json);
+                var data;
+                try {
+                    data = JSON.parse(body);
+                } catch (e) {
+                    data = body;
+                }
+                callback({
+                    "status": response.statusCode,
+                    "data": data
+                });
             });
-        }).end();
+        });
+        if (postdata) {
+            req.write(postdata);
+        }
+        req.end();
     }
 };
 
-module.exports.create = function (method, path) {
-    if (conf == undefined) {
-        conf = JSON.parse(fs.readFileSync('conf/server.json', 'utf8'));
-    }
-    return new Request(method, path)
+module.exports.create = function (method, path, postdata) {
+    return new Request(method, path, postdata)
 };
 
--- spec/tcrequestSpec.js
+++ spec/tcrequestSpec.js
@@ -1,26 +1,19 @@
 "use strict";
 
+GLOBAL.taconf = {
+    "hostname": "localhost",
+    "port": 8888,
+    "username": "admin",
+    "password": "pass"
+};
+
 var mockfs = require('mock-fs');
 var tcrequest = require('../app/tcrequest');
+var nock = require('nock');
 
-describe('tcrequest', function() {
+describe('tcrequest', function () {
 
-    beforeAll(function () {
-        var jsonServer = require('json-server');
-        var server = jsonServer.create();
-        server.use(jsonServer.defaults());
-        var router = jsonServer.router('specdb.json');
-        server.use('/httpAuth/app/rest/', router);
-        server.listen(8888);
-
-        mockfs({
-            conf: {
-                'server.json': '{ "hostname": "localhost", "port" : 8888, "username": "admin", "password": "pass" }'
-            }
-        });
-    });
-
-    it('should read TC config from the server.json file', function() {
+    it('should read TC config from the server.json file', function () {
         var request = tcrequest.create("GET", "/http");
         expect(request.host).toBe('localhost');
         expect(request.port).toBe(8888);
@@ -29,16 +22,45 @@ describe('tcrequest', function() {
         expect(request.path).toBe("/http");
     });
 
-    it('should execute http request', function(done) {
-        var request = tcrequest.create("GET", "/httpAuth/app/rest/test");
-        request.execute(function(json) {
-            expect(json).toEqual({ "result" : "success"});
+    it('should execute http request', function (done) {
+
+        nock('http://localhost:8888')
+            .get('/test')
+            .reply(200, {
+                "result": "success"
+            });
+
+        var request = tcrequest.create("GET", "/test");
+        request.execute(function (result) {
+            expect(result.status).toEqual(200);
+            expect(result.data).toEqual({"result": "success"});
             done();
-        })
+        });
     });
 
-    afterAll(function() {
-        mockfs.restore();
+    it('should survive error while parsing json', function (done) {
+        nock('http://localhost:8888')
+            .get('/test')
+            .reply(500, "Error");
+        var request = tcrequest.create("GET", "/test");
+        request.execute(function (result) {
+            expect(result.status).toEqual(500);
+            done();
+        });
+
+    });
+
+    it('should send postdata', function (done) {
+        nock('http://localhost:8888')
+            .post('/test', {"postdata": "present"})
+            .reply(200, {
+                "result": "success"
+            });
+        var request = tcrequest.create("POST", "/test", '{"postdata": "present"}');
+        request.execute(function (result) {
+            expect(result.status).toEqual(200);
+            done();
+        });
     });
 
 });
